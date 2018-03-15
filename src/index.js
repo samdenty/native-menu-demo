aef85aa6891ecff8f52e7cae55e149f3080f190c
Sam Denty <samddenty@gmail.com>
2018-03-15 16:02:19 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -52,8 +52,32 @@ class Test extends React.Component {
           </div>
         </NativeMenu>
 
-        <NativeMenu theme="material-dark" items={myItems}>
+        <NativeMenu platform="mac" items={myItems}>
           <div className="example">
+            <h1>Platform: mac</h1>
+            <a href="">Example link</a> <button>Example button</button>{' '}
+            <img src="https://via.placeholder.com/900x36" />
+          </div>
+        </NativeMenu>
+
+        <NativeMenu platform="edge" items={myItems}>
+          <div className="example odd">
+            <h1>Platform: edge</h1>
+            <a href="">Example link</a> <button>Example button</button>{' '}
+            <img src="https://via.placeholder.com/900x36" />
+          </div>
+        </NativeMenu>
+
+        <NativeMenu platform="windows" items={myItems}>
+          <div className="example">
+            <h1>Platform: windows</h1>
+            <a href="">Example link</a> <button>Example button</button>{' '}
+            <img src="https://via.placeholder.com/900x36" />
+          </div>
+        </NativeMenu>
+
+        <NativeMenu theme="material-dark" items={myItems}>
+          <div className="example odd">
             <h1>Theme: material-dark</h1>
             <a href="">Example link</a> <button>Example button</button>{' '}
             <img src="https://via.placeholder.com/900x36" />
@@ -61,7 +85,7 @@ class Test extends React.Component {
         </NativeMenu>
 
         <NativeMenu theme="material" items={myItems}>
-          <div className="example odd">
+          <div className="example">
             <h1>Theme: material</h1>
             <a href="">Example link</a> <button>Example button</button>{' '}
             <img src="https://via.placeholder.com/900x36" />
@@ -72,7 +96,7 @@ class Test extends React.Component {
           hide={['search', 'back', 'forward', 'print']}
           items={myItems}
         >
-          <div className="example">
+          <div className="example odd">
             <h1>Hiding specific context menu items</h1>
             <a href="">Example link</a> <button>Example button</button>{' '}
             <img src="https://via.placeholder.com/900x36" />
@@ -87,7 +111,7 @@ class Test extends React.Component {
           }}
           items={myItems}
         >
-          <div className="example odd">
+          <div className="example">
             <h1>Utilizing custom styles</h1>
             <a href="">Example link</a> <button>Example button</button>{' '}
             <img src="https://via.placeholder.com/900x36" />
@@ -95,7 +119,7 @@ class Test extends React.Component {
         </NativeMenu>
 
         <NativeMenu mimic>
-          <div className="example">
+          <div className="example odd">
             <h1>Mimic all the browsers menu items</h1>
             <a href="">Example link</a> <button>Example button</button>{' '}
             <img src="https://via.placeholder.com/900x36" />
@@ -109,7 +133,7 @@ class Test extends React.Component {
           }}
           mimic
         >
-          <div className="example odd">
+          <div className="example">
             <h1>Listen for native menu-item clicks</h1>
             <a href="">Example link</a> <button>Example button</button>{' '}
             <img src="https://via.placeholder.com/900x36" />
@@ -117,7 +141,7 @@ class Test extends React.Component {
         </NativeMenu>
 
         <NativeMenu minimal>
-          <div className="example">
+          <div className="example odd">
             <h1>Minimal amount of context menu items</h1>
             <a href="">Example link</a> <button>Example button</button>{' '}
             <img src="https://via.placeholder.com/900x36" />
